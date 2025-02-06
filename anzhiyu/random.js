ea004bc538b8ededb4dfb51aad5fd1af59ce5e29
杨记瑞 <2957530661@qq.com>
2025-02-06 16:59:55 +0800

    Site updated: 2025-02-06 16:59:55

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/05/hello-world/"];function toRandomPost(){
+var posts=["2025/02/05/hello-world/","2025/02/06/rabbitmq/","2025/02/06/ES/","2025/02/06/zookeeper/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
