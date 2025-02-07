032ba58063b806e97157c3a92deec60cfa412e33
杨记瑞 <2957530661@qq.com>
2025-02-07 13:16:31 +0800

    Site updated: 2025-02-07 13:16:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/05/hello-world/","2025/02/06/rabbitmq/","2025/02/06/ES/","2025/02/06/zookeeper/"];function toRandomPost(){
+var posts=["2025/02/06/ELK搭建/","2025/02/06/ES/","2025/02/07/dockerminio/","2025/02/07/esik/","2025/02/07/mianshi/","2025/02/07/nacosjiqun/","2025/02/06/rabbitmq/","2025/02/07/redispython/","2025/02/06/zookeeper/","2025/02/07/thread1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
